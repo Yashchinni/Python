83922d2dad66eb28d3d061516ffdb076ba43af16
Yashchinni <95892245+Yashchinni@users.noreply.github.com>
2022-03-15 22:49:53 +0530

    Add files via upload

---
+++ CALCULATOR.py
@@ -0,0 +1,10 @@
+#  ..........BASIC CALCULATOR.......
+first_num = 10
+second_num = 20
+third_num = 30
+
+print(first_num + second_num + third_num)  # Addition..
+print(first_num - second_num - third_num)  # Subtration.
+print(first_num * second_num * third_num)  # Multiplication.
+print(first_num / second_num / third_num)  # Division.
+print(first_num + second_num - third_num)  #Add & Sub
---
+++ YASH.py
@@ -0,0 +1,11 @@
+string = "yaswanth"
+letter = "c"
+num = 121234567890
+float_ = 1.2334546563547477
+
+
+# +   -   *   /   %   ^  <  >  <=  >=  ==  !=  
+
+#   ...BOOLEAN...
+True
+False
